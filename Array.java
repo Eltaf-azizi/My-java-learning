9b891b090931a7f271e8574ef6c327a7e91c417b
unknown <iltfhuss@gmail.com>
2024-06-15 23:09:42 +0500

    added java file

---
+++ Array.java
@@ -0,0 +1,17 @@
+
+
+public class Array {
+    public static void main(String a[]){
+        int nums [] = new int [4];
+        nums[0] = 4;
+        nums[1] = 2;
+        nums[2] = 7;
+        nums[3] = 1;
+
+        for(int i=0;i<4;i++){
+            System.out.println(nums[i]);
+        }
+    }
+}
+
+
