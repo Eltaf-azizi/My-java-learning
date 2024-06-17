f9d1424fe7e0fff06fb488b613ee1999c9d6d3fc
unknown <iltfhuss@gmail.com>
2024-06-17 22:07:38 +0500

    added java file

---
+++ hello.java
@@ -0,0 +1,147 @@
+
+
+public class hello {
+    public static void main(String[] arg){
+
+        //class and object
+        // int num1 = 4;
+        // int num2 = 7;
+        
+        // Calculator calc = new Calculator();
+        // int result = calc.add(num1, num2);
+
+        // System.out.println(result);
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Ternaly 
+
+        // int n = 8;
+        // int result = 0;
+
+
+        // result = n%2==0 ? 10 : 20;
+
+        // System.out.println(result);
+
+
+
+
+
+
+
+
+
+        // Switch Statement
+
+        // int n = 4;
+        // switch(n){
+        //     case 1:
+        //         System.out.println("Monday"); 
+        //         break;
+        //     case 2:
+        //         System.out.println("Tuesday");
+        //         break;
+        //     case 3: 
+        //         System.out.println("Wednesday"); 
+        //         break;
+        //     case 4:
+        //         System.out.println("Thusrday"); 
+        //         break;
+        //     case 5:
+        //         System.out.println("Friday"); 
+        //         break;
+        //     case 6:
+        //         System.out.println("Saturday");
+        //         break; 
+        //     case 7:
+        //         System.out.println("Sunday"); 
+        // }
+
+
+
+
+
+
+
+
+
+        
+
+
+
+    //While loop
+    //     int a = 1;
+
+    //     while(a<=1){
+    //         System.out.println("Janguary");
+    //         int i = 1;
+    //         while(i<=30){
+    //             System.out.println("day " + i);
+    //             i++;
+    //         }
+    //         a++;
+    //     }
+
+
+
+
+
+
+
+
+        
+
+
+        
+    //Do while loop
+        // int i = 7;
+
+        // do{
+        //     System.out.println("Hi" + i);
+
+        //     i++;
+
+        // }while(i<=4);
+     
+
+
+        
+
+
+
+
+
+
+        //For loop
+
+        // for(int i=1; i<=4; i++){
+        //     System.out.println("Hi " + i);
+        //     for(int h=1; h<=4; h++){
+        //         System.out.println("   Hi " + h);
+        //     }
+        // }
+
+
+
+
+
+
+
+
+
+
+
+        
+    }
+}
