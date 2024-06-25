fd36ac30c1513b3b831fdbb83ce28178fd210152
unknown <iltfhuss@gmail.com>
2024-06-25 23:18:49 +0500

    added java file

---
+++ string.java
@@ -0,0 +1,12 @@
+
+// navin
+public class string {
+    public static void main(String a[]){
+        StringBuffer sb = new StringBuffer("score ");
+        sb.append("reddy");
+
+        sb.insert(6, "Java ");
+
+        System.out.println(sb);
+    }
+}
